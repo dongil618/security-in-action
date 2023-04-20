eb5eb71343b1f3bc87a291fffb870a051586e798
dongil618 <rlaehddlf618@gmail.com>
2023-04-20 20:32:34 +0900

    feat : 2.3.4 AuthenticationProvider 구현 재정의

--- src/main/java/com/example/securityinaction/config/ProjectConfig.java
+++ src/main/java/com/example/securityinaction/config/ProjectConfig.java
@@ -1,7 +1,11 @@
 package com.example.securityinaction.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,21 +13,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+@RequiredArgsConstructor
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
+    final private CustomAuthenticationProvider authenticationProvider;
 
-        var user = User.withUsername("dongil")
-                .password("1234")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-
-        return userDetailsService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var userDetailsService = new InMemoryUserDetailsManager();
+//
+//        var user = User.withUsername("dongil")
+//                .password("1234")
+//                .authorities("read")
+//                .build();
+//
+//        userDetailsService.createUser(user);
+//
+//        return userDetailsService;
+//    }
 
     /**
      * 암호 처리(해당 Bean이 없으면 java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null" 에러 발생)
@@ -32,5 +44,12 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest().authenticated();  // 모든 요청에 인증이 필요
     }
 }
